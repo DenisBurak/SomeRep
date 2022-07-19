866c17007654025ef3b33a451a86da824749af28
Zemelya <www.pixel93@gmail.com>
2022-07-19 20:19:08 +0300

    initial commit

---
+++ src/main/java/com/zemelya/Test.java
@@ -0,0 +1,13 @@
+package com.zemelya;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        String s = RandomStringUtils.randomAlphabetic(10);
+        System.out.println(s);
+    }
+}
